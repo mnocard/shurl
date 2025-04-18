5db739808fa12e05babe82c7d66e05fce0cf3c7a
Хайрбек Мамбетов <mnocard@gmail.com>
2025-04-18 20:59:25 +0500

    iter1 short url

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,65 @@
 package main
 
-func main() {}
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
+	"net/http"
+)
+
+var addresses = make(map[string]string)
+
+func getHash(b []byte) string {
+	h := sha1.New()
+	h.Write(b)
+	sha := hex.EncodeToString(h.Sum(nil))
+	return sha[0:8]
+}
+
+func addUrl(res http.ResponseWriter, req *http.Request) {
+	if req.Header.Get("content-type") != "text/plain" {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("wrong content-type"))
+		return
+	}
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		res.Write([]byte("bad request"))
+		return
+	}
+
+	hash := getHash(body)
+	addresses[hash] = string(body)
+
+	res.Write([]byte(hash))
+	res.Header().Set("content-type", "text/plain")
+	res.WriteHeader(http.StatusOK)
+}
+
+func getUrl(res http.ResponseWriter, req *http.Request) {
+	hash := req.RequestURI[1:]
+	address := addresses[hash]
+
+	res.Header().Set("content-type", "text/plain")
+	res.Header().Set("Location", address)
+	res.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func shortenerPage(res http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		addUrl(res, req)
+	} else if req.Method == http.MethodGet {
+		getUrl(res, req)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", shortenerPage)
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
