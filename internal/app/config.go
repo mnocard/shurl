57c4ed6c9a17cbf8cb94bd87847e22d9bb13aa39
Хайрбек Мамбетов <mnocard@gmail.com>
2025-05-02 09:24:39 +0500

    iter5 написал тесты для ParseFlag

--- internal/app/config.go
+++ internal/app/config.go
@@ -11,21 +11,30 @@ type Addr struct {
 	FlagBase string
 }
 
+const (
+	envServerAddress = "SERVER_ADDRESS"
+	envBaseURL       = "BASE_URL"
+	flagA            = "a"
+	flagB            = "b"
+	defRunAddr       = ":8080"
+	defBaseAddr      = "http://localhost:8080"
+)
+
 func ParseFlags(config *Addr) {
-	flag.StringVar(&config.FlagRun, "a", ":8080", "address and port to run server")
-	flag.StringVar(&config.FlagBase, "b", "http://localhost:8080", "base address for short url")
-
-	if addr, ok := os.LookupEnv("SERVER_ADDRESS"); ok && addr != "" {
-		config.FlagRun = addr
-		log.Print("addr, ok")
-	}
-
-	if base, ok := os.LookupEnv("BASE_URL"); ok && base != "" {
-		config.FlagBase = base
-		log.Print("base, ok")
-	}
-
+	flag.StringVar(&config.FlagRun, flagA, defRunAddr, "address and port to run server")
+	flag.StringVar(&config.FlagBase, flagB, defBaseAddr, "base address for short url")
 	flag.Parse()
+
+	if addr, ok := os.LookupEnv(envServerAddress); ok && addr != "" {
+		config.FlagRun = addr
+		log.Printf("addr, ok, addr: %s", addr)
+	}
+
+	if base, ok := os.LookupEnv(envBaseURL); ok && base != "" {
+		config.FlagBase = base
+		log.Printf("base, ok, base: %s", base)
+	}
+
 	log.Print("addr: " + config.FlagRun)
 	log.Print("base: " + config.FlagBase)
 }
